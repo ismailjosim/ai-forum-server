f34fa2a0f430fd226b70a161fb68494c6436ee1c
ismailjosim <ismailjosim99@gmail.com>
2025-11-10 11:43:41 +0600

    handle mongoose zod validation error

--- src/app/helpers/handleMongooseValidationError.ts
+++ src/app/helpers/handleMongooseValidationError.ts
@@ -5,14 +5,14 @@ import { TErrorSources, TGenericErrorResponse } from '../interfaces/error.types'
 export const handleZodValidationError = (
 	err: ZodError,
 ): TGenericErrorResponse => {
-	const errorSources: TErrorSources[] = []
-	const errors = Object.values(err?.errors)
-	errors.forEach((item: any) =>
-		errorSources.push({
-			path: `${item.path.slice().reverse().join(' inside ')} is required ❌`,
-			message: item.message,
-		}),
-	)
+	const errorSources: TErrorSources[] = err.issues.map((issue) => ({
+		path:
+			issue.path.length > 0
+				? `${issue.path.slice().reverse().join(' inside ')} is required ❌`
+				: 'Validation failed ❌',
+		message: issue.message,
+	}))
+
 	return {
 		statusCode: 400,
 		message: 'Zod Error Occurred ❌',
